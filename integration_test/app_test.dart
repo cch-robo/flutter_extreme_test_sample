e629f006dc197ca39151e3daa3131603d2c782d7
cch-robo <robo@cch-lab.com>
2021-10-16 17:12:03 +0900

    Integration test を元のパターンに戻す。 （テスト時に ViewModelを外部参照可能にするエクストリーム(極端) 設定を除外）

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,27 +1,9 @@
 import 'package:extreme_test_sample/main.dart' as app;
-import 'package:extreme_test_sample/swappable_instance_factory.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
-  late final app.CounterViewModel viewModel;
-
-  setUp(() {
-    // ViewModel をテスト中に外部参照可能にするため、
-    // テスト中にインスタンス差替可能なファクトリに、CounterViewModel を設定。
-    Factory.setTesting(true);
-    viewModel = app.CounterViewModel();
-    Factory.setSwapInstance<app.CounterViewModel>(viewModel,
-        id: "CounterViewModel");
-  });
-
-  tearDown(() {
-    // テスト中にインスタンス差替可能なファクトリの設定をクリア。
-    Factory.clear();
-    Factory.setTesting(false);
-  });
 
   group('end-to-end test', () {
     testWidgets('tap on the floating action button, verify counter',
@@ -31,7 +13,6 @@ void main() {
 
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
-      print("before increment - counter = ${viewModel.counter}");
 
       // Finds the floating action button to tap on.
       final Finder fab = find.byTooltip('Increment');
@@ -44,7 +25,6 @@ void main() {
 
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
-      print("after increment - counter = ${viewModel.counter}");
     });
   });
 }
