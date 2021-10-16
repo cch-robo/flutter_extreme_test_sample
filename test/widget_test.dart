e794a547dd3235788fd010eb3c11ffa2c218ff59
cch-robo <robo@cch-lab.com>
2021-10-16 17:09:35 +0900

    Widget test に Riverpod ProviderScope が含まれるよう、ウィジェットツリールートを修正

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,15 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:extreme_test_sample/main.dart' as app;
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:extreme_test_sample/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // riverpod ProviderScope 定義を含むよう、アプリ全体のウィジェットツリーを作成します。
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(app.providerScope);
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
