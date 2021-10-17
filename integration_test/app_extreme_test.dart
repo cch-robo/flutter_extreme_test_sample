16336fcd6f30009b5307562018916d15bb5c21aa
cch-robo <robo@cch-lab.com>
2021-10-17 14:34:23 +0900

    group関数内でテストを複数定義できるように修正

--- integration_test/app_extreme_test.dart
+++ integration_test/app_extreme_test.dart
@@ -6,25 +6,25 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  /// テスト中に差し替えて、外部から参照可能にする ViewModel
-  late final app.CounterViewModel viewModel;
-
-  setUp(() {
-    // ViewModel をテスト中に外部参照可能にするため、
-    // テスト中にインスタンス差替可能なファクトリに、CounterViewModel を設定。
-    Factory.setTesting(true);
-    viewModel = app.CounterViewModel();
-    Factory.setSwapInstance<app.CounterViewModel>(viewModel,
-        id: 'CounterViewModel');
-  });
-
-  tearDown(() {
-    // テスト中にインスタンス差替可能なファクトリの設定をクリア。
-    Factory.clear();
-    Factory.setTesting(false);
-  });
-
   group('end-to-end extreme test', () {
+    /// テスト中に差し替えて、外部から参照可能にする ViewModel
+    late app.CounterViewModel viewModel;
+
+    setUp(() {
+      // ViewModel をテスト中に外部参照可能にするため、
+      // テスト中にインスタンス差替可能なファクトリに、CounterViewModel を設定。
+      Factory.setTesting(true);
+      viewModel = app.CounterViewModel();
+      Factory.setSwapInstance<app.CounterViewModel>(viewModel,
+          id: 'CounterViewModel');
+    });
+
+    tearDown(() {
+      // テスト中にインスタンス差替可能なファクトリの設定をクリア。
+      Factory.clear();
+      Factory.setTesting(false);
+    });
+
     // インスタンス差替可能ファクトリを利用して、ViewModel を外部参照可能にした検証
     testWidgets('tap on the floating action button, extreme verify counter',
         (WidgetTester tester) async {
